e1cc4be054eb56838a52d4539b0c616952efb983
Ni-ChiTing <dfdf22222@gmail.com>
2019-06-15 17:55:12 +0800

    add android arr and detect AP or Wifi

--- New Unity Project/Assets/Scripts/StartScene.cs
+++ New Unity Project/Assets/Scripts/StartScene.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Android;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -8,10 +10,12 @@ public class StartScene : MonoBehaviour
 {
     Text Name;
     static GameObject Messagebox;
+    AndroidJavaObject _ajc;
     public void ClientBtn() {
         Data.IamHost = false;
-        
+        bool success3 = isWifiApEnabled();
         if (string.IsNullOrEmpty(Name.text)){
+            
             Messagebox = (GameObject)Resources.Load("Simple UI/MessageBox");
             Messagebox = GameObject.Instantiate(Messagebox,GameObject.Find("Canvas").transform) as GameObject;
             Messagebox.transform.localScale = new Vector3(1, 1, 1);            
@@ -22,10 +26,46 @@ public class StartScene : MonoBehaviour
             Messagebox.GetComponent<MessageBoxControll>().Title.text = "Warning";
             Messagebox.GetComponent<MessageBoxControll>().Close.onClick.AddListener(Close_btn);
             Messagebox.GetComponent<MessageBoxControll>().Confirm.onClick.AddListener(Close_btn);
+            
+
         }
         else{
             Data.MyName = Name.text;
-            SceneManager.LoadScene(1);
+            bool IfWifiOpen = false;
+            if (Application.internetReachability == NetworkReachability.NotReachable)
+            {
+                //Change the Text
+               // m_ReachabilityText = "Not Reachable.";
+            }
+            //Check if the device can reach the internet via a carrier data network
+            else if (Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork)
+            {
+               // m_ReachabilityText = "Reachable via carrier data network.";
+            }
+            //Check if the device can reach the internet via a LAN
+            else if (Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork)
+            {
+                //m_ReachabilityText = "Reachable via Local Area Network.";
+                IfWifiOpen = true;
+            }
+            if (!IfWifiOpen)
+            {
+                Messagebox = (GameObject)Resources.Load("Simple UI/MessageBox");
+                Messagebox = GameObject.Instantiate(Messagebox, GameObject.Find("Canvas").transform) as GameObject;
+                Messagebox.transform.localScale = new Vector3(1, 1, 1);
+                Messagebox.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+                Messagebox.GetComponent<RectTransform>().offsetMin = Vector2.zero;
+                Messagebox.GetComponent<RectTransform>().offsetMax = Vector2.zero;
+                Messagebox.GetComponent<MessageBoxControll>().Content.text = "Please Connect Host !!";
+                Messagebox.GetComponent<MessageBoxControll>().Title.text = "Warning";
+                Messagebox.GetComponent<MessageBoxControll>().Close.onClick.AddListener(Close_btn);
+                Messagebox.GetComponent<MessageBoxControll>().Confirm.onClick.AddListener(Close_btn);
+            }
+            else
+            {
+                SceneManager.LoadScene(1);
+            }
+            
         }
         
     }
@@ -44,10 +84,33 @@ public class StartScene : MonoBehaviour
             Messagebox.GetComponent<MessageBoxControll>().Title.text = "Warning";
             Messagebox.GetComponent<MessageBoxControll>().Close.onClick.AddListener(Close_btn);
             Messagebox.GetComponent<MessageBoxControll>().Confirm.onClick.AddListener(Close_btn);
+            
+            //bool success = isWifiApEnabled();
+            
+            //bool success3 = isWifiApEnabled();
+            //bool success23 = _ajc.Call<bool>("showToast", success3.ToString());
         }
         else{
-            Data.MyName = Name.text;
-            SceneManager.LoadScene(3);
+            bool success = _ajc.Call<bool>("CheckWifiAP");
+            bool success2 = _ajc.Call<bool>("showToast", success.ToString());
+            if (success){
+                Data.MyName = Name.text;
+                SceneManager.LoadScene(3);
+            }
+            else
+            {
+                Messagebox = (GameObject)Resources.Load("Simple UI/MessageBox");
+                Messagebox = GameObject.Instantiate(Messagebox, GameObject.Find("Canvas").transform) as GameObject;
+                Messagebox.transform.localScale = new Vector3(1, 1, 1);
+                Messagebox.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+                Messagebox.GetComponent<RectTransform>().offsetMin = Vector2.zero;
+                Messagebox.GetComponent<RectTransform>().offsetMax = Vector2.zero;
+                Messagebox.GetComponent<MessageBoxControll>().Content.text = "Please Open Wifi AP";
+                Messagebox.GetComponent<MessageBoxControll>().Title.text = "Warning";
+                Messagebox.GetComponent<MessageBoxControll>().Close.onClick.AddListener(Close_btn);
+                Messagebox.GetComponent<MessageBoxControll>().Confirm.onClick.AddListener(Close_btn);
+
+            }
         }
     }
     public void Close_btn() {
@@ -55,10 +118,39 @@ public class StartScene : MonoBehaviour
     }
     public void Start()
     {
-        Name = GameObject.Find("Canvas/InputField/Text").GetComponent<Text>(); 
+        
+        _ajc = new AndroidJavaObject("com.androidforunity.UnityFuntion");
+        Debug.Log(_ajc);
+        Name = GameObject.Find("Canvas/InputField/Text").GetComponent<Text>();
+        //bool success = _ajc.Call<bool>("RequestPermission");
     }
     public void Update()
     {
       
     }
+    public void FromAndroid(string content)
+    {
+        Debug.Log("ACL");
+    }
+
+    public bool isWifiApEnabled()
+    {
+        Debug.Log("SSs");
+        using (AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"))
+        {
+            try
+            {
+                using (var wifiManager = activity.Call<AndroidJavaObject>("getSystemService", "wifi"))
+                {
+                    Debug.Log("SSS");
+                    return wifiManager.Call<bool>("isWifiApEnabled");
+                }
+            }
+            catch (Exception e)
+            {
+            }
+        }
+        return false;
+    }
+
 }
---
+++ UnityAAR/androidforunity/src/main/java/com/androidforunity/UnityFuntion.java
@@ -0,0 +1,92 @@
+package com.androidforunity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class UnityFuntion {
+    private Activity _unityActivity;
+
+    Activity getActivity() {
+        if (null == _unityActivity) {
+            try {
+                Class<?> classtype = Class.forName("com.unity3d.player.UnityPlayer");
+                Activity activity = (Activity) classtype.getDeclaredField("currentActivity").get(classtype);
+                _unityActivity = activity;
+            } catch (ClassNotFoundException e) {
+
+            } catch (IllegalAccessException e) {
+
+            } catch (NoSuchFieldException e) {
+
+            }
+        }
+        return _unityActivity;
+    }
+
+    boolean callUnity(String gameObjectName, String functionName, String args) {
+        try {
+            Class<?> classtype = Class.forName("com.unity3d.player.UnityPlayer");
+            Method method = classtype.getMethod("UnitySendMessage", String.class, String.class, String.class);
+            method.invoke(classtype, gameObjectName, functionName, args);
+            return true;
+        } catch (ClassNotFoundException e) {
+
+        } catch (NoSuchMethodException e) {
+
+        } catch (IllegalAccessException e) {
+
+        } catch (InvocationTargetException e) {
+
+        }
+        return false;
+    }
+
+    public boolean showToast(String content) {
+        Toast.makeText(getActivity(), content, Toast.LENGTH_SHORT).show();
+        //这里是主动调用Unity中的方法，该方法之后unity部分会讲到
+        callUnity("Canvas", "FromAndroid", "hello unity i'm android");
+        return true;
+    }
+
+    public boolean CheckWifiAP() {
+
+        WifiManager wifi = (WifiManager) getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        try {
+            Method method = wifi.getClass().getMethod("isWifiApEnabled");
+            method.setAccessible(true);
+            return (Boolean) method.invoke(wifi);
+
+        } catch (NoSuchMethodException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return  false;
+    }
+    public boolean RequestPermission(){
+
+        int permissionCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_WIFI_STATE);
+        Toast.makeText(getActivity(), "已經拿到權限囉!", Toast.LENGTH_SHORT).show();
+        //沒有權限時
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getActivity(), "NO 權限!", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_WIFI_STATE},
+                    1);
+        } else {
+            Toast.makeText(getActivity(), "已經拿到權限囉!", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+}
