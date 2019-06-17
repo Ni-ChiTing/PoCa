ac9ae6c32da11e693dffdb88b9fb3dab766f9d26
HoeYuan <onghoeyuan0214@outlook.com>
2019-06-18 01:06:05 +0800

    auto update cardnum in setting

--- New Unity Project/Assets/Scripts/Setting.cs
+++ New Unity Project/Assets/Scripts/Setting.cs
@@ -11,8 +11,7 @@ using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Setting : MonoBehaviour
-{
+public class Setting : MonoBehaviour {
     Text player2;
     Text player3;
     Text player4;
@@ -27,19 +26,19 @@ public class Setting : MonoBehaviour
     private AndroidJavaObject _ajc;
     string IPs;
     int PlayerCount;
-    Socket socket; 
-    EndPoint serverEnd; 
+    Socket socket;
+    EndPoint serverEnd;
     IPEndPoint ipEnd;
     string recvStr;
-    string sendStr; 
+    string sendStr;
     byte[] recvData = new byte[1024];
     byte[] sendData = new byte[1024];
-    int recvLen; 
-    Thread connectThread; 
+    int recvLen;
+    Thread connectThread;
     int UdpPort = 10230;
     //Object thisLock = new Object();
     bool IfGo = false;
-    enum InfoState {HostName, PlayerNumber , PlayerCardNumber , TableCardNumber , NeedDrawCard, NeedAnimation, Players, Done };
+    enum InfoState { HostName, PlayerNumber, PlayerCardNumber, TableCardNumber, NeedDrawCard, NeedAnimation, Players, Done };
     InfoState state;
     /*SocketSend(Data.HostName);
                 SocketSend(Data.PlayerNumber.ToString());
@@ -47,33 +46,44 @@ public class Setting : MonoBehaviour
                 SocketSend(Data.TableCardNumber.ToString());
                 SocketSend(Data.NeedDrawCard.ToString());
                 SocketSend(Data.NeedAnimation.ToString());*/
-    public string getsubnet()
-    {
+
+    Dropdown gameModeDropDown;
+    Dropdown playerNumDropDown;
+    InputField playerCardNum;
+
+    public string getsubnet() {
         string hostName = System.Net.Dns.GetHostName();
         string ipBase = System.Net.Dns.GetHostEntry(hostName).AddressList[0].ToString();
         string[] ipParts = ipBase.Split('.');
-        ipBase = ipParts[0] + "." + ipParts[1] + "." + ipParts[2] ;
+        ipBase = ipParts[0] + "." + ipParts[1] + "." + ipParts[2];
         return ipBase;
     }
-    public void Start()
-    {
+    public void Start() {
         _ajc = new AndroidJavaObject("com.androidforunity.UnityFuntion");
         player2 = GameObject.Find("Canvas/conn/connect1/Text").GetComponent<Text>();
         player3 = GameObject.Find("Canvas/conn/connect2/Text").GetComponent<Text>();
         player4 = GameObject.Find("Canvas/conn/connect3/Text").GetComponent<Text>();
-        game_name = GameObject.Find("Canvas/set/bg/game/Dropdown/Label").GetComponent<Text>();
-        player_number = GameObject.Find("Canvas/set/bg/set/pn/Dropdown/Label").GetComponent<Text>();
-        player_card_number = GameObject.Find("Canvas/set/bg/set/cn/InputField/Text").GetComponent<Text>();
+        game_name = GameObject.Find("Canvas/set/bg/game/GameModeDropdown/Label").GetComponent<Text>();
+        player_number = GameObject.Find("Canvas/set/bg/set/pn/PlayerNumberDropDown/Label").GetComponent<Text>();
+        player_card_number = GameObject.Find("Canvas/set/bg/set/cn/CardNumberInputField/Text").GetComponent<Text>();
         //table_card_number = GameObject.Find("Canvas/set/bg/set/TableCardInputField/Text").GetComponent<Text>();
         need_animation = GameObject.Find("Canvas/set/bg/toggle/Toggle").GetComponent<Toggle>();
         need_draw_card = GameObject.Find("Canvas/set/bg/toggle/Toggle1").GetComponent<Toggle>();
+
+        gameModeDropDown = GameObject.Find("Canvas/set/bg/game/GameModeDropdown").GetComponent<Dropdown>();
+        playerNumDropDown = GameObject.Find("Canvas/set/bg/set/pn/PlayerNumberDropDown").GetComponent<Dropdown>();
+        playerCardNum = GameObject.Find("Canvas/set/bg/set/cn/CardNumberInputField").GetComponent<InputField>();
+
+        gameModeDropDown.onValueChanged.AddListener(delegate { gameMode_onChange(); });
+        playerNumDropDown.onValueChanged.AddListener(delegate { playerNumber_onChange(); });
+
+        gameMode_onChange();
     }
-    public void startBtn()
-    {
+    public void startBtn() {
         int temp;
         Data.GameName = game_name.text;
         Data.PlayerNumber = int.Parse(player_number.text); ;
-        print("Player num = "+ Data.PlayerNumber.ToString());
+        print("Player num = " + Data.PlayerNumber.ToString());
         Data.players.Add(player2.text);
         Data.players.Add(player3.text);
         Data.players.Add(player4.text);
@@ -81,38 +91,24 @@ public class Setting : MonoBehaviour
         else Data.NeedAnimation = false;
         if (need_draw_card.isOn == true) Data.NeedDrawCard = true;
         else Data.NeedDrawCard = false;
-        if (int.TryParse(player_card_number.text,out temp) == false)
-        {
+        if (int.TryParse(player_card_number.text, out temp) == false) {
             ShowDia("warning", "Player card number need number !!");
-        }
-        else
-        {
+        } else {
             Data.PlayerCardNumber = int.Parse(player_card_number.text);
-            if (int.TryParse(table_card_number.text, out temp) == false)
-            {
-                ShowDia("warning", "Table card number need number !!");
-            }
-            else
-            {
-                ShowDiaData();
-                Data.TableCardNumber = int.Parse(table_card_number.text);
-                StartTransmitData();
-                GameObject.Destroy(Messagebox);
-                SendStart();
-                SceneManager.LoadScene(2);
-            }
-        }        
-        
+            ShowDiaData();
+            StartTransmitData();
+            GameObject.Destroy(Messagebox);
+            SendStart();
+            SceneManager.LoadScene(2);
+        }
+
     }
-    public void StartTransmitData()
-    {
+    public void StartTransmitData() {
         string[] ipParts = IPs.Split(',');
         string hostName = System.Net.Dns.GetHostName();
         string ipBase = System.Net.Dns.GetHostEntry(hostName).AddressList[0].ToString();
-        for (int i = 0; i < ipParts.Length - 1; ++i)
-        {
-            if (ipParts[i] != ipBase)
-            {
+        for (int i = 0; i < ipParts.Length - 1; ++i) {
+            if (ipParts[i] != ipBase) {
                 Debug.Log(ipParts[i]);
                 ipEnd = new IPEndPoint(IPAddress.Parse(ipParts[i]), UdpPort);
                 socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
@@ -124,32 +120,26 @@ public class Setting : MonoBehaviour
                 SendAndAckReceive();
                 socket.Close();
             }
-
         }
-        
     }
-    public void ReceiveStartACK()
-    {
+
+    public void ReceiveStartACK() {
         recvData = new byte[1024];
         recvLen = socket.ReceiveFrom(recvData, ref serverEnd);
         recvStr = Encoding.ASCII.GetString(recvData, 0, recvLen);
-        while (recvStr != "ACK")
-        {
+        while (recvStr != "ACK") {
             SocketSend("GO");
             recvData = new byte[1024];
             recvLen = socket.ReceiveFrom(recvData, ref serverEnd);
             recvStr = Encoding.ASCII.GetString(recvData, 0, recvLen);
         }
-    } 
-    public void SendAndAckReceive()
-    {
+    }
+    public void SendAndAckReceive() {
         int index = 0;
-        while (state != InfoState.Done)
-        {
+        while (state != InfoState.Done) {
             print(state);
             string str = "";
-            switch (state)
-            {
+            switch (state) {
                 case InfoState.HostName:
                     str = Data.HostName;
                     break;
@@ -173,8 +163,7 @@ public class Setting : MonoBehaviour
             recvData = new byte[1024];
             recvLen = socket.ReceiveFrom(recvData, ref serverEnd);
             recvStr = Encoding.ASCII.GetString(recvData, 0, recvLen);
-            switch (state)
-            {
+            switch (state) {
                 case InfoState.HostName:
                     if (recvStr == "ACK")
                         state = InfoState.NeedAnimation;
@@ -196,12 +185,9 @@ public class Setting : MonoBehaviour
                         state = InfoState.Players;
                     break;
                 case InfoState.Players:
-                    if (index == Data.PlayerNumber-1 )
-                    {
+                    if (index == Data.PlayerNumber - 1) {
                         state = InfoState.Done;
-                    }
-                    else
-                    {
+                    } else {
                         print("index" + index.ToString());
                         ++index;
                         state = InfoState.Players;
@@ -213,16 +199,13 @@ public class Setting : MonoBehaviour
         print("SSS");
     }
 
-    void SendStart()
-    {
+    void SendStart() {
         print("START SEND");
         string[] ipParts = IPs.Split(',');
         string hostName = System.Net.Dns.GetHostName();
         string ipBase = System.Net.Dns.GetHostEntry(hostName).AddressList[0].ToString();
-        for (int i = 0; i < ipParts.Length - 1; ++i)
-        {
-            if (ipParts[i] != ipBase)
-            {
+        for (int i = 0; i < ipParts.Length - 1; ++i) {
+            if (ipParts[i] != ipBase) {
                 Debug.Log(ipParts[i]);
                 ipEnd = new IPEndPoint(IPAddress.Parse(ipParts[i]), UdpPort);
                 socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
@@ -232,21 +215,19 @@ public class Setting : MonoBehaviour
                 //print("waiting for sending UDP dgram");
                 state = InfoState.HostName;
                 SocketSend("GO");
-                #if DEBUG
+#if DEBUG
                 ReceiveStartACK();
-                #endif
+#endif
                 socket.Close();
             }
 
         }
     }
-    public void Player_btn()
-    {
+    public void Player_btn() {
         ShowDiaResearch();
         //GameObject.Destroy(Messagebox);
     }
-    public void ShowDia(string title,string context)
-    {
+    public void ShowDia(string title, string context) {
         Messagebox = (GameObject)Resources.Load("Simple UI/MessageBox");
         Messagebox = GameObject.Instantiate(Messagebox, GameObject.Find("Canvas").transform) as GameObject;
         Messagebox.transform.localScale = new Vector3(1, 1, 1);
@@ -258,8 +239,7 @@ public class Setting : MonoBehaviour
         Messagebox.GetComponent<MessageBoxControll>().Close.onClick.AddListener(Close_btn);
         Messagebox.GetComponent<MessageBoxControll>().Confirm.onClick.AddListener(Close_btn);
     }
-    public void ShowDiaResearch()
-    {
+    public void ShowDiaResearch() {
         Messagebox = (GameObject)Resources.Load("Simple UI/MessageBox_r");
         Messagebox = GameObject.Instantiate(Messagebox, GameObject.Find("Canvas").transform) as GameObject;
         Messagebox.transform.localScale = new Vector3(1, 1, 1);
@@ -271,8 +251,7 @@ public class Setting : MonoBehaviour
         Messagebox.GetComponent<MessageBoxControll>().Close.onClick.AddListener(Close_btn);
         Messagebox.GetComponent<MessageBoxControll>().Confirm.onClick.AddListener(Do_clk);
     }
-    public void ShowDiaData()
-    {
+    public void ShowDiaData() {
         Messagebox = (GameObject)Resources.Load("Simple UI/MessageBox_r");
         Messagebox = GameObject.Instantiate(Messagebox, GameObject.Find("Canvas").transform) as GameObject;
         Messagebox.transform.localScale = new Vector3(1, 1, 1);
@@ -284,13 +263,11 @@ public class Setting : MonoBehaviour
         Messagebox.GetComponent<MessageBoxControll>().Close.onClick.AddListener(Close_btn);
         Messagebox.GetComponent<MessageBoxControll>().Confirm.gameObject.SetActive(false);
     }
-    public void DoMes()
-    {
+    public void DoMes() {
         Messagebox.GetComponent<MessageBoxControll>().Confirm.gameObject.SetActive(false);
         Messagebox.GetComponent<MessageBoxControll>().Content.text = "Scanning .. .. ..";
     }
-    public void Do_clk()
-    {
+    public void Do_clk() {
         //bool success23 = _ajc.Call<bool>("showToast", "wait search!!");
 
         //DoMes();
@@ -300,15 +277,13 @@ public class Setting : MonoBehaviour
         setPlayers(IPs);
         PrintINFO();
         GameObject.Destroy(Messagebox);
-        
+
     }
-    public void Close_btn()
-    {
+    public void Close_btn() {
         //string a = _ajc.Call<string>("startPingService", getsubnet());
         GameObject.Destroy(Messagebox);
     }
-    public void setPlayers(string scannresult)
-    {
+    public void setPlayers(string scannresult) {
         Data.playerIP.Clear();
         Data.players.Clear();
         string[] ipParts = scannresult.Split(',');
@@ -316,22 +291,19 @@ public class Setting : MonoBehaviour
         string ipBase = System.Net.Dns.GetHostEntry(hostName).AddressList[0].ToString();
         Data.players.Add(Data.MyName);
         Data.playerIP.Add(ipBase);
-        for ( int i = 0; i < ipParts.Length - 1; ++ i )
-        {
-            if ( ipParts[i] != ipBase)
-            {
+        for (int i = 0; i < ipParts.Length - 1; ++i) {
+            if (ipParts[i] != ipBase) {
                 Debug.Log(ipParts[i]);
                 InitSocket(ipParts[i]);
-                
+
             }
-            
+
         }
     }
-    
+
     //初始化
-    void InitSocket(string ip)
-    {
-        ipEnd = new IPEndPoint(IPAddress.Parse(ip),UdpPort);
+    void InitSocket(string ip) {
+        ipEnd = new IPEndPoint(IPAddress.Parse(ip), UdpPort);
         socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         socket.ReceiveTimeout = 1000;
         IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
@@ -342,15 +314,13 @@ public class Setting : MonoBehaviour
     }
 
 
-    void SocketSend(string sendStr)
-    {
+    void SocketSend(string sendStr) {
         sendData = new byte[1024];
         sendData = Encoding.ASCII.GetBytes(sendStr);
         socket.SendTo(sendData, sendData.Length, SocketFlags.None, ipEnd);
     }
 
-    void SocketReceive()
-    {
+    void SocketReceive() {
         string data = "";
         recvData = new byte[1024];
         recvLen = socket.ReceiveFrom(recvData, ref serverEnd);
@@ -360,26 +330,19 @@ public class Setting : MonoBehaviour
         Data.playerIP.Add(realip[0]);
         recvStr = Encoding.ASCII.GetString(recvData, 0, recvLen);
         data += "," + recvStr;
-            if (PlayerCount == 0)
-            {
-                player2.text = recvStr;
+        if (PlayerCount == 0) {
+            player2.text = recvStr;
             //++PlayerCount;
-            }
-            else if(PlayerCount == 1)
-            {
-                player3.text = recvStr;
+        } else if (PlayerCount == 1) {
+            player3.text = recvStr;
             //++PlayerCount;
-            }
-            else if(PlayerCount == 2)
-            {
-                player4.text = recvStr;
-            
-            }
-            else
-            {
-                Debug.Log("player is full");
-            }
-            ++PlayerCount;
+        } else if (PlayerCount == 2) {
+            player4.text = recvStr;
+
+        } else {
+            Debug.Log("player is full");
+        }
+        ++PlayerCount;
         print(recvStr);
         /*
         while (true)
@@ -410,8 +373,7 @@ public class Setting : MonoBehaviour
         SocketQuit();
     }
 
-    void SocketQuit()
-    {
+    void SocketQuit() {
         /*
         if (connectThread != null)
         {
@@ -421,31 +383,62 @@ public class Setting : MonoBehaviour
         if (socket != null)
             socket.Close();
     }
-    public void PrintINFO()
-    {
+    public void PrintINFO() {
         print("Hostname = " + Data.MyName);
         print("Player num = " + PlayerCount.ToString());
         print("Host IP = " + Data.HostIP);
         int j = 0;
-        foreach (var i in Data.players)
-        {
+        foreach (var i in Data.players) {
             print("Player " + j.ToString() + " = " + i);
             ++j;
         }
         j = 0;
-        foreach (var i in Data.playerIP)
-        {
+        foreach (var i in Data.playerIP) {
             print("Player IP" + j.ToString() + " = " + i);
             ++j;
         }
     }
-    void Update()
-    {
-        
+    void Update() {
+
     }
-    void OnApplicationQuit()
-    {
+    void OnApplicationQuit() {
         SocketQuit();
+    }
+
+    private void gameMode_onChange() {
+        print("gameMode_onChange(): " + gameModeDropDown.value);
+        switch (gameModeDropDown.value) {
+            case 0: // 21點
+                print("gameMode: 21點");
+                playerCardNum.text = "2";
+                break;
+            case 1: // 大老二
+                print("gameMode: 大老二");
+                playerCardNum.text = "13";
+                break;
+            case 2: // 抽鬼牌
+                print("gameMode: 抽鬼牌");
+                playerCardNum.text = "13";
+                break;
+            case 3: // 橋牌
+                print("gameMode: 橋牌");
+                playerCardNum.text = "13";
+                break;
+            case 4: // 牌7
+                print("gameMode: 牌7");
+                playerCardNum.text = "13";
+                break;
+        };
+        updateSetting();
+    }
+
+    private void playerNumber_onChange() {
+        print("playerNumber_onChange(): " + player_number.text);
+        updateSetting();
+    }
+
+    private void updateSetting() {
+        //table_card_number.text = (52 - int.Parse(player_number.text) * int.Parse(playerCardNum.text)).ToString();
     }
 }
 
